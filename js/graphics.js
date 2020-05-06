c1fafee0a6367460f5e19bf7fd5a48451567282f
Alessio Verdolini <alessio.verdolini@gmail.com>
2020-05-07 00:02:01 +0200

    Daje

---
+++ js/graphics.js
@@ -0,0 +1,68 @@
+width = 600;
+height = 350;
+
+
+/* Initialize canvas */
+svg = d3.select(".wrapper")
+    .append("svg")
+    .classed("canvas", true)
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("viewBox", "0 0 " + width + " " + height + "")
+    .attr("preserveAspectRatio", "xMidYMid meet");
+
+function draw(batsArrangements) {
+    var bats = svg.selectAll(".bat")
+        .data(batsArrangements)
+        .enter().append("image")
+        .classed("bat", true)
+        .attr('xlink:href', 'resources/alive.svg')
+        .attr('id', function (d) {return "bat" + batsArrangements.indexOf(d)})
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr('x', function (d) {return d.x})
+        .attr('y', function (d) {return d.y})
+        .on("click", function(e, i){
+            block(i)
+        });
+}
+
+function updateArrangement(batsArrangements) {
+    svg.selectAll(".bat")
+        .data(batsArrangements)
+        .transition()
+        .duration(1000)
+        .attr('x', function (d, i) {return setPositionX(i, d)})
+        .attr('y', function (d, i) {return setPositionY(i, d)});
+}
+
+function block(i) {
+    svg.select("#bat"+i)
+        .attr("blocked", "true")
+        .attr('xlink:href', 'resources/dead.svg')
+    alive--;
+    console.log(alive)
+    if(alive===0) {
+        console.log("callInit")
+        svg.selectAll(".bat")
+            .data(batsArrangements)
+            .exit()
+            .remove();
+        init();
+    }
+
+}
+
+function setPositionX(i, d) {
+    if(svg.select("#bat"+i).attr("blocked"))
+        return svg.select("#bat"+i).attr("x");
+    else
+        return d.x
+}
+
+function setPositionY(i, d) {
+    if(svg.select("#bat"+i).attr("blocked"))
+        return svg.select("#bat"+i).attr("y");
+    else
+        return d.y;
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
-width = 600;
-height = 350;
+let dataset = [];
+let batsArrangements = [];
+let contatore = 0;
+let alive = 6;
 
-/* Initialize canvas */
-svg = d3.select(".wrapper")
-    .append("svg")
-    .classed("canvas", true);
+function init() {
+    contatore = 0;
+    alive = 6;
+    setTimeout(function (){
+        d3.json("resources/position.json",).then(function (data) {
+            dataset = data;
+            loadData();
+            draw(batsArrangements["0"]);
+            addEventListener();
+        });
+    }, 1000);
+}
+
+function loadData() {
+    batsArrangements = dataset['arrangements'];
+}
+
+function addEventListener() {
+    d3.select("body").on("keydown", function () {
+        if (d3.event.keyCode === 88) {
+
+            if(contatore === 4) {
+                contatore = 0
+            }else {
+                contatore++
+            }
+            updateArrangement(batsArrangements[contatore])
+        }
+    });
+}
+
+
+init();
+
 
